be5711a857f50ecf04c01d0f74e5f0be7784711b
inwonakng <inwon.kang04@gmail.com>
2023-12-09 10:21:38 -0500

    moved contract project

---
+++ contracts/src/Buyer.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {LinkTokenInterface} from "@chainlink/contracts-ccip/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+
+contract Buyer {
+    address link;
+    address router;
+
+    constructor(address _link, address _router) {
+        link = _link;
+        router = _router;
+        LinkTokenInterface(link).approve(router, type(uint256).max);
+    }
+
+    function send(address receiver, string memory content, uint64 destinationChainSelector) external {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver),
+            data: abi.encode(content),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: link
+        });
+
+        IRouterClient(router).ccipSend(destinationChainSelector, message);
+    }
+
+    function receiveMsg(
+        Client.Any2EVMMessage memory any2EvmMessage
+    ) internal override {
+        s_lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
+        s_lastReceivedText = abi.decode(any2EvmMessage.data, (string)); // abi-decoding of the sent text
+
+        emit MessageReceived(
+            any2EvmMessage.messageId,
+            any2EvmMessage.sourceChainSelector, // fetch the source chain identifier (aka selector)
+            abi.decode(any2EvmMessage.sender, (address)), // abi-decoding of the sender address,
+            abi.decode(any2EvmMessage.data, (string))
+        );
+    }
+
+    function sendBuyRequest() {}
+      // send the buy request
+    }
+
+    function verifyProof() {
+        // prove the zk proof
+
+    }
+}
--- contracts/src/Seller.sol
+++ contracts/src/Seller.sol
@@ -5,7 +5,7 @@ import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.s
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {LinkTokenInterface} from "@chainlink/contracts-ccip/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 
-contract CCIPSender_Unsafe {
+contract Seller {
     address link;
     address router;
 
@@ -14,16 +14,36 @@ contract CCIPSender_Unsafe {
         router = _router;
         LinkTokenInterface(link).approve(router, type(uint256).max);
     }
-    
-    function send(address receiver, string memory someText, uint64 destinationChainSelector) external {
+
+    function send(address receiver, string memory content, uint64 destinationChainSelector) external {
         Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
             receiver: abi.encode(receiver),
-            data: abi.encode(someText),
+            data: abi.encode(content),
             tokenAmounts: new Client.EVMTokenAmount[](0),
             extraArgs: "",
             feeToken: link
         });
 
         IRouterClient(router).ccipSend(destinationChainSelector, message);
+    }
+
+    function receiveMsg(
+        Client.Any2EVMMessage memory any2EvmMessage
+    ) internal override {
+        s_lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
+        s_lastReceivedText = abi.decode(any2EvmMessage.data, (string)); // abi-decoding of the sent text
+
+        emit MessageReceived(
+            any2EvmMessage.messageId,
+            any2EvmMessage.sourceChainSelector, // fetch the source chain identifier (aka selector)
+            abi.decode(any2EvmMessage.sender, (address)), // abi-decoding of the sender address,
+            abi.decode(any2EvmMessage.data, (string))
+        );
+    }
+
+
+    function generateProof() {
+        // generate the zk proof
+
     }
 }
